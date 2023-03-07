88a6c07c5321875efa0b4d12f4dcd69fccc7ce95
Wilberforce Sedem Haibor <wmaggickk@gmail.com>
2023-03-07 02:02:17 +0000

    Complete Files

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageNotFoundImage from '../images/pagenotfound.png';
+
+
+export const PageNotFound = () => {
+  
+  useEffect(() => {
+    document.title = `Page Not Found / BlockBuster`;
+  })
+  
+  return (
+    <main>
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my-4">
+          <p className="text-7xl text-gray-700 font-bold my-10 dark:text-white">
+            404, Oops!
+          </p>
+          <div className="max-w-lg">
+            <img
+              className="rounded"
+              src={PageNotFoundImage}
+              alt="404 page not found"
+            />
+          </div>
+        </div>
+        <div className='flex justify-center my-4'>
+          <Link to="/">
+            <button className='w-64 text-xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-2 font-medium'>Back to BlockBuster</button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+};
